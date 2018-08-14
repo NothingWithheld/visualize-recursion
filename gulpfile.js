01acab49ad60604483b4542da140fece866353ad
Andrew Hsu <nothingwithheldsocial@gmail.com>
2018-08-13 19:46:04 -0500

    Separate styles for recursive modules

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,16 @@ const del = require('del');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 
-gulp.task('sass', function() {
+gulp.task('sass-main', function() {
     gulp.src('./src/styles/main.scss')
         .pipe(sass())
         .pipe(gulp.dest('./dist/styles'));
+});
+
+gulp.task('sass-modules', function() {
+    gulp.src('./src/styles/recursive_modules/*')
+        .pipe(sass())
+        .pipe(gulp.dest('./dist/styles/recursive_modules'));
 });
 
 gulp.task('clean', function() {
