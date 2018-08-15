83104e7f1f3b6b39bc80cbb455dc0e2d24beb8f1
Andrew Hsu <nothingwithheldsocial@gmail.com>
2018-08-14 22:32:00 -0500

    Add rename + build + default

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ const cssnano = require('cssnano');
 const del = require('del');
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
+const rename = require('gulp-rename');
+const runSequence = require('run-sequence');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
@@ -15,9 +17,12 @@ gulp.task('js-modules', function() {
         .pipe(sourcemaps.init())
         .pipe(babel())
         .pipe(uglify())
+        .pipe(rename((path) => path.basename += '.min'))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./dist/scripts/recursive_modules'));
 });
+
+gulp.task('js', ['js-modules']);
 
 gulp.task('sass-main', function() {
     return gulp.src('./src/styles/main.scss')
@@ -27,6 +32,7 @@ gulp.task('sass-main', function() {
             autoprefixer(),
             cssnano()
         ]))
+        .pipe(rename((path) => path.basename += '.min'))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./dist/styles'));
 });
@@ -39,6 +45,7 @@ gulp.task('sass-modules', function() {
             autoprefixer(),
             cssnano()
         ]))
+        .pipe(rename((path) => path.basename += '.min'))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./dist/styles/recursive_modules'));
 });
@@ -48,3 +55,13 @@ gulp.task('sass', ['sass-main', 'sass-modules']);
 gulp.task('clean', function() {
     return del('./dist/**/*');
 });
+
+gulp.task('build', function() {
+    return runSequence('clean', ['js', 'sass']);
+})
+
+gulp.task('default', ['build'], function() {
+    gulp.watch('./src/scripts/recursive_modules/*', ['js-modules'])
+    gulp.watch(['./src/styles/**/*.scss', '!./src/styles/recursive_modules/*'], ['sass-main'])
+    gulp.watch('./src/styles/recursive_modules/*', ['sass-modules'])
+})
