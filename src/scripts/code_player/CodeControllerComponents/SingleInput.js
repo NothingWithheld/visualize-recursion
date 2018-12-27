ff0c332451849699367a9eb34681a75f22e14de5
Andrew Hsu <nothingwithheldsocial@gmail.com>
2018-12-26 22:09:57 -0600

    Add single input component

---
+++ src/scripts/code_player/CodeControllerComponents/SingleInput.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import React from 'react';
+
+class SingleInput extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.props.handleChange(event);
+    }
+
+    render() {
+        return (
+            <label>
+                {this.props.label}
+                <input 
+                    onChange={this.handleChange} 
+                    name={this.props.name}
+                    value={this.props.value}
+                />
+            </label>
+        );
+    }
+}
+
+export default SingleInput;
