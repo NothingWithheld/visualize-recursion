c0c9bd434e75c5ac0b6689aa597c4ce283bfce7d
Andrew Hsu <nothingwithheldsocial@gmail.com>
2020-07-03 10:58:46 -0500

    Add VariablesWindow

---
+++ src/CodePlayer/RecursionCanvas/VariablesWindow/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Option, getOrElse } from 'fp-ts/es6/Option'
+import Box from '@material-ui/core/Box'
+import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+
+const CodeText = withStyles({
+	root: {
+		backgroundColor: 'rgba(27,31,35,.05)',
+		fontFamily: 'Roboto Mono, monospace',
+	},
+})(Typography)
+
+interface VariablesWindowProps {
+	readonly args: Array<[string, string]>
+	readonly variableDetails: Array<[string, string[]]>
+	readonly returnValue: Option<string>
+}
+
+const VariablesWindow = ({
+	args,
+	variableDetails,
+	returnValue,
+}: VariablesWindowProps): JSX.Element => {
+	const assignedVariables = variableDetails.filter(
+		([, valList]) => valList.length > 0
+	)
+
+	return (
+		<Box display="flex" margin="14px 8px" padding="20px">
+			<Typography>Arguments</Typography>
+			{args.map(([name, val], i) => (
+				<CodeText key={i}>{`${name} = ${val}`}</CodeText>
+			))}
+			{assignedVariables.length > 0 && (
+				<>
+					<Typography>Variables</Typography>
+					{assignedVariables.map(([name, valList], i) => (
+						<CodeText key={i}>{`${name} = ${
+							valList[valList.length - 1]
+						}`}</CodeText>
+					))}
+				</>
+			)}
+			<Typography>Return Value</Typography>
+			<CodeText>
+				{getOrElse(() => "Function hasn't yet returned")(returnValue)}
+			</CodeText>
+		</Box>
+	)
+}
+
+export default VariablesWindow
